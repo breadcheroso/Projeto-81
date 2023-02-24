1e402d028f7d86927e29b7fee0a9e44ab456b7bf
breadcheroso <102551644+breadcheroso@users.noreply.github.com>
2023-02-24 14:51:14 -0300

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Feed from "../screens/Feed";
+import CreatePost from "../screens/CreatePost";
+
+const BottomTabNavigator = () => {
+    return (
+<Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === 'Feed') {
+            iconName = focused
+                ? 'book'
+                : 'book-outline';
+        } else if (route.name === 'CratePost') {
+            iconName = focused ? 'create' : 'create-outline';
+        }
+        return <Ionicons name={iconName} size={size} color={color} />;
+    },
+})}
+tabBarOptions={{
+    activeTintColor: 'tomato',
+    inactiveTintColor: 'gray',
+}}
+>
+<Tab.Screen name="Feed" component={Feed} options={{headerShown:false}}/>
+      <Tab.Screen name="Criar Post" component={CreateStory} options={{headerShown:false}}/>
+    </Tab.Navigator>
+)}
